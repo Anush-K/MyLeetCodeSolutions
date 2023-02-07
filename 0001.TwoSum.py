c7003e3382b71b4ad4d4f49683f46b4456d0ef8d
Anush-K <73292232+Anush-K@users.noreply.github.com>
2023-02-07 22:31:23 +0530

    Update 0001.TwoSum.py

--- 0001.TwoSum.py
+++ 0001.TwoSum.py
@@ -1,3 +1,14 @@
+"""
+Question
+--------
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+"""
+
+"""
+Answer
+------
+"""
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         result = []
